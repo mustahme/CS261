ab55faabb1ad30e264ec9e1aef5dd6d4c80f0dcc
ahmed <mustahme@oregonstate.edu>
2023-10-05 22:54:56 -0700

    Push

--- assignment0_debugger.py
+++ assignment0_debugger.py
@@ -1,5 +1,13 @@
+# Name: Ahmed Mustafa
+# OSU Email: MustaAhm@OregonState.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 09OCT2023
+# Description: This will be used to verify GradeScope is working for CS261
+
+
 # Please enter your name, favorite color, favorite hobby, and hometown in the list
-my_list = ['', '', '', '']
+my_list = ['Ahmed Mustafa', 'Blue', 'Hiking', 'Harrisonburg VA']
 
 def my_info(my_list):
     """ A function that passes a list of my information """
