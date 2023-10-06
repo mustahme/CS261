2236f8b0e46dbf703d5ad9d2327de3c48bb3502f
ahmed <mustahme@oregonstate.edu>
2023-10-05 22:53:47 -0700

    Push

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Ahmed Mustafa
+# OSU Email: MustaAhm@OregonState.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 09OCT2023
+# Description: This will be used to verify GradeScope is working for CS261
+
+
+def gradescope_test():
+    return 42
